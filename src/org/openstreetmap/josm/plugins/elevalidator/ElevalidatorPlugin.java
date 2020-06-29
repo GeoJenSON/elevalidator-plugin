ca9bdf7e633ab44c68f24e1410d6a2455d3d1bd4
Fabian Jenzen <44211901+GeoJenSON@users.noreply.github.com>
2020-06-29 16:25:12 +0200

    First gui version without functionality

--- src/org/openstreetmap/josm/plugins/elevalidator/ElevalidatorPlugin.java
+++ src/org/openstreetmap/josm/plugins/elevalidator/ElevalidatorPlugin.java
@@ -1,13 +1,19 @@
 package org.openstreetmap.josm.plugins.elevalidator;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.openstreetmap.josm.gui.IconToggleButton;
 import org.openstreetmap.josm.gui.MapFrame;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
 
+
 public class ElevalidatorPlugin extends Plugin {
 
     // Will be invoked by JOSM to bootstrap the plugin
+
+    private static final Logger LOGGER = Logger.getLogger(ElevalidatorPlugin.class.getName());
 
     protected static ElevalidatorDialog elevalidatorDialog;
 
@@ -23,6 +29,8 @@ public class ElevalidatorPlugin extends Plugin {
 
         if (oldFrame == null && newFrame != null) {
             newFrame.addToggleDialog(elevalidatorDialog = new ElevalidatorDialog());
+            //LOGGER.log(Level.INFO, elevalidatorDialog);
+            System.out.println("Test logging");
         }
     }
 }
